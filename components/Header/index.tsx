dde69d9c878db8b0c220195b8ded76f144d5c8bc
Badrú <sbsbadroo@gmail.com>
2024-11-30 17:46:50 +0200

    fit: estrutura

---
+++ components/Header/index.tsx
@@ -0,0 +1,17 @@
+export const Header = () => {
+    return(
+        <div className="bg-black w-full py-3">
+        <h1> testando Header</h1>
+        <nav>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Perfil
+                </li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
