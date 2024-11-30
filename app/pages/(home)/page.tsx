53b1deff49210e8dab45bccdc4f1f9408632c82f
Badrú <sbsbadroo@gmail.com>
2024-11-30 17:53:24 +0200

    fit: home

---
+++ app/pages/(home)/page.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/Header"
+
+const Home = () =>{
+    return (
+        <div>
+            <Header/>
+
+
+            
+            {/* teu codigo daqui pra baixo*/}
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Home;
