37d57b411191684fff69046683d4d46c83f6ff46
yurannhassengo <yurannhassengo@gmail.com>
2024-12-01 09:13:10 -0800

    add DeleteRoute

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,4 +1,4 @@
-import { updateUser } from "@/services/usuarios";
+import { deleteUser, getUserById, updateUser } from "@/services/usuarios";
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -19,3 +19,42 @@ export async function PUT(
         );
     }
 };
+
+export async function DELETE(
+    request:NextRequest,
+    {params}: {params: Promise<{id: string}>}
+){
+    const {id} = await params;
+    try{
+        const deletedUser = await deleteUser(id);
+        return NextResponse.json(deletedUser);
+    }catch(error){
+        return NextResponse.json(
+            {error: `Error ao remover o usuario: ${error}`},
+            {status:500}
+        );
+    }
+};
+
+export async function GET (
+    request: NextRequest,
+    {params}: {params: Promise<{id: string}>}
+){
+    const {id} = await params;
+
+    try{
+        const user = await getUserById(id);
+
+        if(!user){
+            return NextResponse.json({error: `Usuario nao encontrado`},{status:404});
+        }
+
+        return NextResponse.json(user);
+    }catch(error) {
+        return NextResponse.json(
+            {error: `Error ao Buscar o usuario: ${error}`},
+            {status:500}
+        );
+    }
+}
+
