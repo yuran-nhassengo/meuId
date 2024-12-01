ad2a0730dec34774c184d71565c4acfab034b9ff
yurannhassengo <yurannhassengo@gmail.com>
2024-12-01 08:50:53 -0800

    add PostRoute

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,4 @@
-import { getAllUser } from "@/services/usuarios";
+import { createUser, getAllUser } from "@/services/usuarios";
 import { NextResponse } from "next/server";
 
 
@@ -11,3 +11,16 @@ export async  function GET() {
     }
 } 
 
+export async function POST(request: Request) {
+    const body = await request.json();
+
+    try {
+        const {nome,email,senha} = body;
+        const newUser = await createUser(nome,email,senha);
+        return NextResponse.json(newUser,{status: 201});
+
+    }catch (error){
+        return NextResponse.json({error: `Error ${error}`},{status: 500});
+
+    }
+}
